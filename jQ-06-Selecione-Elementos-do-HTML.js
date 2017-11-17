fb5557b65fbae7089e8ae2079de7b2b587cf84f8
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-17 14:35:07 -0200

    Selecione elemento do HTML.

---
+++ jQ-06-Selecione-Elementos-do-HTML.js
@@ -0,0 +1,22 @@
+
+$(function(){
+
+    $("button")  
+
+});
+
+
+/* $('button') - seleciona todos os elementos com o nome button 
+   $('#botao1') - selecinona apenas o botão 1
+   $('.botao') - selecionas as classes botao
+
+   $('li.botao') - seleciona apenas a tag li com a classe botao
+
+   $('.lista2 li.botao) 
+        |------ vou entrar na lista2 , e na linha com a classe botao 
+
+    $('tag') - seleciona por tag
+    $('.nomeDaClasse') - seleciona por classe nome
+    $('#nomeDoId') - seleciona por id nome
+    
+*/
