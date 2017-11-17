d9d04734bae866b3d065da8dbb8dac5772be05bc
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-17 11:38:39 -0200

    Evitando conflito.

---
+++ jQ-05-Evitando-conflito.js
@@ -0,0 +1,15 @@
+/* Acontece quando utlizamos a biblioteca prototype javascript */
+
+
+ /* var $j = jQuery.noConflict();  
+  passamos a ulizar j$ ao invés de $ 
+
+$j(document).ready(function(){
+    alert("Está funcionando...")
+});
+*/
+
+/* Usando o jQuery Nativamente */
+jQuery(document).ready(function(){
+    alert("Funcionando Nativamente...");
+});
