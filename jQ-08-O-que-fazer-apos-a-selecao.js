e493ce18e75c4a7bc92b069113068949783da7d2
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-17 19:00:29 -0200

    Acessando e mudando propriedades.

---
+++ jQ-08-O-que-fazer-apos-a-selecao.js
@@ -0,0 +1,12 @@
+ $(function(){
+
+    /* $('#teste').html("Texto trocado"); */
+
+    $('#teste').find('button').addClass('estilo');
+    /* pinta o botão de vermelho */
+
+    $('#teste').find('button').html("Novo nome do botão");
+    /* muda o nome do botão */
+    
+}); 
+
