9b9bd0a1ce6d907bbd156990db15112ad0c1959e
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-18 17:59:39 -0200

    CSS, Styling e Dimensões.

---
+++ jQ-12-CSS-Styling-Dimensões.js
@@ -0,0 +1,41 @@
+/*
+    <h1>Página do curso de jQuery.</h1>
+    <h2>CSS, Styling e Dimensões</h2>
+    <p>Exemplos de CSS, Styling e Dimensões.</p>
+    <span>Nome:</span><br><br>
+    <input type="text" name="nome"><br><br>
+    <"button>Botão</button>
+
+Adicionar fundo ao <h1>:
+$('h1').addClass("fundoVermelho");
+
+Remover fundo do <h1>:
+$('h1').removeClass("fundoVermelho");
+
+Verifica se tem alguma classe:
+$('h1').hasClass('fundoVermelho');  --retorna true ou false.
+
+if( $('h1').hasClass('fundoVermelho') ) {
+    alert("Tem classe");
+    } else {
+        alert("Não tem classe");
+    }
+
+Adicionando CSS:
+$('h1').css("color", "red");
+$('h1').css('font-size', '30px');
+
+$('h1').css("color", "red").css('font-size', '30px');
+
+
+
+
+
+
+
+
+
+
+
+
+*/
