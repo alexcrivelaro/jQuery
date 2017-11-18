2783a232184724e18c795d0ece288e4e2e5e9d35
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-18 21:12:31 -0200

    Como saber se um elemento existe.

---
+++ jQ-15-Como-saber-se-um-elemento-existe.js
@@ -0,0 +1,11 @@
+/*
+O jQuery sempre retornará um array.
+É preciso verificar se esse array é maior que zero
+para algo existir.
+if($('#palavra').length > 0 ) {
+    alert("EXISTE");
+} else {
+    alert("NÃO EXITE");
+}
+
+*/
