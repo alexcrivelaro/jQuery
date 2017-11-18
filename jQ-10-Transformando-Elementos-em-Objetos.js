0f79dc5ec7e0bfa5363c631c86261221264ec253
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-18 10:28:24 -0200

    Transformando elementos em objetos.

---
+++ jQ-10-Transformando-Elementos-em-Objetos.js
@@ -0,0 +1,47 @@
+/* $(function(){
+
+    
+}); */
+
+/*
+Selecionar o h1:
+$('h1');
+
+Selecionar o h1 e trocar o conteúdo:
+$('h1').html("Título novo");
+
+Transferir o h1 para uma variável:
+var titulo = $('h1');
+
+Manipulando o texto com a nova variável:
+titulo.html("Título Novo 2");
+
+Selecionando o elemento por javascript:
+document.getElementById("palavra");
+
+Posso guardar o elemento em uma variável:
+var word = document.getElementById("palavra");
+
+Manipulando diretamente no JavaScript
+$(document.getElementById("palavra")).html("Novo parágrafo aplicado");
+
+Manipulando pelo jQuery
+$(div).html("Novo parágrafo aplicado");
+
+selecionar o segundo item da lista abaixo
+<ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+    <li>Item 4</li>
+</ul>
+$('li:eq(1)');       ou
+$('li').eq(1);
+
+mudando a <li>Item 2<li>   para <li>Item segundo<li>
+$('li').eq(1).html("Item segundo");
+
+
+
+
+*/
