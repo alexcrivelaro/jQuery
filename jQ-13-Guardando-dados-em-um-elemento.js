48bdf35f421040ca3fda82988cddf368d91251c2
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-18 19:00:19 -0200

    Guardando dados em um elemento.

---
+++ jQ-13-Guardando-dados-em-um-elemento.js
@@ -0,0 +1,16 @@
+/*
+Guardar dados no input:
+$('input').attr('data-idade', '90');
+
+Recuperar esse dado:
+$('input').data('idade');
+esse valor está associado na memória pelo jQuery
+
+Atribuir o método ao input de contar carcteres:
+$('input').data('carcteres', $('input').val().length);
+
+Recuperar esse valor:
+$('input').data('carcteres')
+
+
+*/
