d9195ea0c96c873dc50c43e873a7f628fd3356d4
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-18 06:23:59 -0200

    Manipulando elementos

---
+++ jQ-09-Manipulando-Elementos.js
@@ -0,0 +1,35 @@
+ $(function(){
+
+    
+}); 
+
+/*
+$('#teste').html("Esse é <strong>negrito</strong>");
+- nesse caso adiciona somente o html - Esse é negrito
+
+$('#teste').text("Esse é <strong>negrito</strong>");
+- agora adiciona tudo - Esse é <strong>negrito</strong>
+
+$('#teste').find('img').attr('width', '100') ou
+$('#teste').find('img').width(100)
+
+
+Para adicionar valor ao input
+$('input').attr('value', 'Esse é um teste');
+ou
+$('input').val('teste') - mais indicada
+
+Inserir um elemento:
+$('input').after("<div>Texto qualquer</div>");
+$('input').before("<div>Texto qualquer</div>");
+
+Adicionar um item a lista: (adiciona o que já existe)
+$('ul').append("<li>Item 5</li>");
+
+$('ul').html("<li>Item 5</li>"); - nesse caso ele substitui
+
+$('ul').prepend("<li>Item 0</li>"); - adiciona um item anterior
+
+$('input').remove(); - remove o item da tela
+
+*/
