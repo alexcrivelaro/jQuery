4d12fcd1ede73c328e8fa62fb359f92b9d3205f8
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-19 08:43:58 -0200

    Alterar 3º elemento de lista.

---
+++ jQ-19-Alterar-3o-elemento-de-lista.js
@@ -0,0 +1,17 @@
+/*
+Selecionando o item 3 de uma lista de 10 elementos
+$('li').eq(2);
+
+Alterando o item 3:
+$('li').eq(2).html("Item alterado").css('color', 'green');
+
+Removendo o item 6:
+$('li').eq(5).remove();
+
+
+
+
+
+
+
+*/
