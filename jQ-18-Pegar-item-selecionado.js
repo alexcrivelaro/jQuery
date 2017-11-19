ec235345ab5362ddf10081544ec17a560d0582c4
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-19 08:20:37 -0200

    Como pegar um item selecionado de SELECT.

---
+++ jQ-18-Pegar-item-selecionado.js
@@ -0,0 +1,10 @@
+/*
+Como pegar um item selecionado de SELECT.
+$('#idade').val()
+
+ou ainda:
+$('#idade').find('option').eq(1);
+
+Para selecionar um item ainda não selecionado:
+$('#idade').find('option').eq(1).attr('selected', 'selected');
+*/
