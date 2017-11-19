e58201cfe194d9a8b091817adc4d72a60da27ed1
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-19 10:45:25 -0200

    Troca da classe utilizando hover e toggleClass.

---
+++ jQ-23-Eventos-Auxiliadores.js
@@ -0,0 +1,20 @@
+$(function(){
+
+    $('button').hover(function(){
+        $(this).toggleClass('fundoVermelho');
+
+/*    $('button').hover(function(){
+        $(this).addClass('fundoVermelho');
+    }, function(){
+        $(this).remoceClass('fundoVermelho');
+    });
+*/
+ /* $('button').mouseover(function(){
+        $(this).addClass('fundoVermelho');
+    });
+
+    $('button').mouseout(function(){
+        $(this).removeClass('fundoVermelho');
+    });
+*/
+});
