32de46a9dc979dbada039939770e1b94f223bc03
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-20 06:22:50 -0200

    Elemento aparecer e desaparecer.

---
+++ jQ-32-Elemento-aparecer-e-desaparecer.js
@@ -0,0 +1,26 @@
+$(function(){
+
+    $('.botao').bind('click', function(){
+        $('.div').toggle('fast');
+    
+    });
+
+});
+
+
+
+
+/*
+Desaparecer:
+$('.div').hide('fast');
+$('.div').hide('slow');
+
+Aparecer:
+$('.div').show('fast');
+$('.div').show('slow');
+
+Aparecer e desaparecer alternadamente:
+$('.div').toggle();
+$('.div').toggle('fast');
+$('.div').toggle('slow');
+*/
