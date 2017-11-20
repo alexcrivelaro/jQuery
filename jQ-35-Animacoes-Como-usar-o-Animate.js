efde61ba9d86dde4678e6d8d2ac0d243ea427858
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-20 08:48:53 -0200

    Animações - Como usar o Animate.

---
+++ jQ-35-Animacoes-Como-usar-o-Animate.js
@@ -0,0 +1,17 @@
+$(function(){
+
+    $('.botao').bind('click', function(){
+       
+        $('.div').animate({
+            'margin-left':200,
+            'margin-top':30,
+            'width':150,
+            'border-radius':75 
+        }, 1500 );
+    });
+
+});
+
+
+
+
